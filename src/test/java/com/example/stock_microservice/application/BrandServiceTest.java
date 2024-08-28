a3026314454a6ef310a97bbbd7123e961c9a81c1
Lau36 <laura.jaimes@correounivalle.edu.co>
2024-08-28 18:50:50 -0500

    Testing

--- src/test/java/com/example/stock_microservice/application/BrandServiceTest.java
+++ src/test/java/com/example/stock_microservice/application/BrandServiceTest.java
@@ -3,6 +3,9 @@ package com.example.stock_microservice.application;
 import com.example.stock_microservice.application.services.BrandService;
 import com.example.stock_microservice.domain.models.Brand;
 import com.example.stock_microservice.domain.ports.input.IBrandUseCase;
+import com.example.stock_microservice.domain.utils.PaginatedBrands;
+import com.example.stock_microservice.domain.utils.PaginationRequest;
+import com.example.stock_microservice.domain.utils.SortDirection;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
@@ -10,9 +13,10 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 
-import static org.mockito.Mockito.verify;
+import java.util.Arrays;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 class BrandServiceTest {
     @Mock
@@ -37,5 +41,16 @@ class BrandServiceTest {
 
         assertEquals(brand, createdBrand);
         verify(brandUseCase, Mockito.times(1)).createBrand(brand);
+    }
+
+    @Test
+    void testListAllBrands(){
+        PaginationRequest paginationRequest = new PaginationRequest(0, 10, "name", SortDirection.ASC);
+        PaginatedBrands paginatedBrands = new PaginatedBrands(Arrays.asList(brand,brand),0,4,2);
+        when(brandUseCase.listAllBrands(paginationRequest)).thenReturn(paginatedBrands);
+
+        PaginatedBrands result = brandService.listAllBrands(paginationRequest);
+        assertEquals(paginatedBrands, result);
+        verify(brandUseCase, times(1)).listAllBrands(paginationRequest);
     }
 }
