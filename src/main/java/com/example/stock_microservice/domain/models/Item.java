090e93098634c8a713ee83601f8f32200743564c
Lau36 <laura.jaimes@correounivalle.edu.co>
2024-08-29 10:22:54 -0500

    model for item

---
+++ src/main/java/com/example/stock_microservice/domain/models/Item.java
@@ -0,0 +1,8 @@
+package com.example.stock_microservice.domain.models;
+
+public class Article {
+    private final Long id;
+    private final String name;
+    private final String description;
+    private final String
+}
